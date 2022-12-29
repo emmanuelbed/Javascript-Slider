68ae27f1fe650bb891676a17b000ef7b622034f8
Yashma <jnre371@gmail.com>
2022-12-29 05:58:39 -0600

    Adding functions for left arrow as well

--- slider.js
+++ slider.js
@@ -12,14 +12,32 @@ const nextSlide = () => {
 };
 
 const prevSlide = () => {
+  slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`;
+  slideNumber--;
+};
+
+const getFirstSlide = () => {
   slider.style.transform = `translateX(0px)`;
   slideNumber = 1;
+};
+
+const getLastSlide = () => {
+  slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
+  slideNumber = length;
 };
 
 right.addEventListener("click", () => {
   if (slideNumber < images.length) {
     nextSlide();
   } else {
+    getFirstSlide();
+  }
+});
+
+left.addEventListener("click", () => {
+  if (slideNumber > 1) {
     prevSlide();
+  } else {
+    getLastSlide();
   }
 });

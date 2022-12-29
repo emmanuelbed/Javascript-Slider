90d45fdd5f972d80f985277f9c3e2d7709a2f657
Yashma <jnre371@gmail.com>
2022-12-29 05:32:47 -0600

    Making slider return to slide 1 after the last slide is scrolled.

--- slider.js
+++ slider.js
@@ -1,10 +1,16 @@
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".image");
 
 let slideNumber = 1;
 
 right.addEventListener("click", () => {
-  slider.style.transform = `translateX(-${slideNumber * 800}px)`;
-  slideNumber++;
+  if (slideNumber < images.length) {
+    slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+    slideNumber++;
+  } else {
+    slider.style.transform = `translateX(0px)`;
+    slideNumber = 1;
+  }
 });

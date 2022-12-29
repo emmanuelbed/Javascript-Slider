e7bbe9fae6ee358f519d3be72e10b3b9ac3499a6
Yashma <jnre371@gmail.com>
2022-12-29 05:25:03 -0600

    Making the Slides move with JS

--- slider.js
+++ slider.js
@@ -1,1 +1,10 @@
-console.log("It works!");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+
+let slideNumber = 1;
+
+right.addEventListener("click", () => {
+  slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+  slideNumber++;
+});
